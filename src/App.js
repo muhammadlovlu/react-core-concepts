0e39b91f7233e5a802249ad0ed8012b8c37a7cad
muhammadlovlu <chyshahab@gmail.com>
2021-04-06 09:30:00 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Dan Russel",
+    Job: "Software Engineer",
+    Status: "Single"
+  }
+  const secondPerson = {
+    name: "Elizabeth",
+    Job: "Nurse",
+    Status: "Married",
+    salary: "$1400"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +20,12 @@ function App() {
         <p>
           React Core Concepts
         </p>
-        <p>hello world</p>
+        <h1>{secondPerson.name + " ," + " " + "Job: " + secondPerson.Job + "," + " " + "Salary:" + secondPerson.salary}</h1>
         <h1>this is a heading typed in react application</h1>
+        <h2> {person.name}: {(2 + 5) * 25 + 65} </h2>
+        <h3>{person.Job}</h3>
+        <h4>{person.Status}</h4>
+
       </header>
     </div>
   );
